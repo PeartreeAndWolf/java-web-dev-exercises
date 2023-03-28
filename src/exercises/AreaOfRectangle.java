5d4b62708da75cb952b86e2ed53a296e6ae86ab5
Ruth Poirier <125494680+PeartreeAndWolf@users.noreply.github.com>
2023-03-27 19:31:01 -0500

    Class 1 exercises and studios completed

---
+++ src/exercises/AreaOfRectangle.java
@@ -0,0 +1,14 @@
+package exercises;
+import java.util.Scanner;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the width of the rectangle: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the length of the rectangle: ");
+        int b = sc.nextInt();
+        int c = a * b;
+        System.out.println("Total= " + c);
+    }
+}
---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,21 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius: ");
+
+        if (!input.hasNextDouble()) {
+            System.out.println("The radius must be a valid number.");
+            return;
+        }
+        double radius = input.nextDouble();
+
+        double area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+    }
+}
