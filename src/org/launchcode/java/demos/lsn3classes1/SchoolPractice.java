9b550d0efbbc7fc7da236c84613311743bbc04f4
Ruth Poirier <125494680+PeartreeAndWolf@users.noreply.github.com>
2023-04-03 15:17:53 -0500

    Class 3/Chpt 4 exercises. Completed, edited, but still throwing cautions bc not using most of the methods, and final and static states of stuff

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,98 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student student = new Student();
+        student.setName("Ruth");
+        student.setStudentId(515);
+        student.setNumberOfCredits(1);
+        student.setGpa(4.0);
     }
+
+    public class Course {
+        private String topic;
+        private String teacher;
+        private ArrayList<Student> enrolledStudents;
+
+        public Course(String topic, String teacher) {
+            this.topic = topic;
+            this.teacher = teacher;
+            this.enrolledStudents = new ArrayList<>();
+        }
+        public String getTopic() {
+            return topic;
+        }
+
+        public void setTopic(String topic) {
+            this.topic = topic;
+        }
+
+        public String getTeacher() {
+            return teacher;
+        }
+
+        public void setTeacher(String teacher) {
+            this.teacher = teacher;
+        }
+
+        public ArrayList<Student> getEnrolledStudents() {
+            return enrolledStudents;
+        }
+
+        public void addStudent(Student student) {
+            enrolledStudents.add(student);
+        }
+    }
+
+    public class Teacher {
+
+        private String firstName;
+        private String lastName;
+        private String subject;
+        private int yearsTeaching;
+
+        public Teacher(String firstName, String lastName, String subject, int yearsTeaching) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.subject = subject;
+            this.yearsTeaching = yearsTeaching;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public void setSubject(String subject) {
+            this.subject = subject;
+        }
+
+        public int getYearsTeaching() {
+            return yearsTeaching;
+        }
+
+        public void setYearsTeaching(int yearsTeaching) {
+            this.yearsTeaching = yearsTeaching;
+        }
+    }
+
 }
+
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -5,9 +5,43 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
 
-    private String name;
-    private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String name;
+    public int studentId;
+    public int numberOfCredits = 0;
+    public double gpa = 0.0;
 
 }
+
+
